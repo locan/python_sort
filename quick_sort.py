713002b03850517f5462faf1689e58efda7308b8
luanpeng <luanpeng@b.360.cn>
2017-08-09 09:33:46 +0800

    归并排序和快排

--- quick_sort.py
+++ quick_sort.py
@@ -2,7 +2,7 @@
 
 
 def sort(lists):
-    return quick_sort(lists, 0, len(lists)-1)
+    return quick_sort(lists, 0, len(lists) - 1)
 
 
 def quick_sort(lists, left, right):
@@ -25,6 +25,21 @@ def quick_sort(lists, left, right):
     quick_sort(lists, low, left - 1)
     quick_sort(lists, left + 1, high)
     return lists
+
+
+def quick_sort_copy(lists):
+    if len(lists) <= 1:
+        return lists
+    key = lists[0]
+    l1 = quick_sort_copy([i for i in lists if i < key])
+    l2 = quick_sort_copy([i for i in lists if i >= key])
+    return l1 + l2
+
+
+def _quick_sort(L):
+    if len(L) <= 1:
+        return L
+    return _quick_sort([lt for lt in L[1:] if lt < L[0]]) + [L[0]] + _quick_sort([ge for ge in L[1:] if ge >= L[0]])
 
 
 if __name__ == '__main__':
